1e99a1e915bb4719d443d7b5da7fa815472dbbd6
Thomas Hammer <584962@stud.hvl.no>
2019-10-28 12:00:33 +0100

    Addet prosjektplaner og nedlastning

---
+++ script.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    document.getElementById("vis1").addEventListener("click", function(event) {
+        let dom = document.getElementById("pdf1");
+        console.log(dom);
+        if(dom.style.display == "inline") {
+            dom.style.display = "none";
+            document.getElementById("vis1").innerHTML = "Vis prosjektplan";
+        } else {
+            dom.style.display = "inline";
+            document.getElementById("vis1").innerHTML = "Skjul prosjektplan";
+        }
+    });
+    document.getElementById("vis2").addEventListener("click", function(event) {
+        let dom = document.getElementById("pdf2");
+        console.log(dom);
+        if(dom.style.display == "inline") {
+            dom.style.display = "none";
+            document.getElementById("vis2").innerHTML = "Vis akademisk versjon";
+        } else {
+            dom.style.display = "inline";
+            document.getElementById("vis2").innerHTML = "Skjul akademisk versjon";
+        }
+    });
+};
